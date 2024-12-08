43925a9e70998a913569805ae69a6d8d4aa3d1aa
Aryan Ramani <aryanramani67@gmail.com>
2024-12-08 20:56:35 +0000

    Added ID, ability to delete and restrcuture

--- go.mod
+++ go.mod
@@ -2,4 +2,5 @@ module github.com/notaryanramani/todo_go
 
 go 1.23.4
 
-require github.com/spf13/pflag v1.0.5
+require github.com/spf13/pflag v1.0.5 
+require github.com/google/uuid v1.6.0 
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"strings"
 	"time"
-
+	uuid "github.com/google/uuid"
 	flag "github.com/spf13/pflag"
 )
 
@@ -41,20 +41,20 @@ func makeCSV(fileName string, force bool) {
 		return
 	}
 	writer := csv.NewWriter(file)
-	writer.Write([]string{"Task", "Time"})
+	writer.Write([]string{"ID","Task", "Time"})
 	writer.Flush()
 	defer file.Close()
 	fmt.Println("File created successfully")
 }
 
-func displayFile(fileName string) {
+func listFile(fileName string) {
 	/*
-		Function to display the contents of a CSV file
+		Function to list the contents of a CSV file
 
 		Parameters:
 			fileName (string): The name of the file to display
 	*/
-	fmt.Println("Displaying file: ", fileName)
+	fmt.Println("Listing file: ", fileName)
 	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
@@ -85,36 +85,104 @@ func writeLine(fileName string, text string) {
 			text (string): The text to write to the file
 	*/
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+	id := uuid.New()
 	if err != nil {
 		panic(err)
 	}
-	data := []string{text, time.Now().Format("2006-01-02 15:04:05")}
+	data := []string{id.String(), text, time.Now().Format("2006-01-02 15:04:05")}
 	writer := csv.NewWriter(file)
 	writer.Write(data)
 	writer.Flush()
 }
 
+func deleteTask(filename string, id string){
+	/*
+		Function to delete a task from a CSV file
+
+		Parameters:
+			fileName (string): The name of the file to delete from
+			id (string): The ID of the task to delete
+	*/
+	file, err := os.OpenFile(filename, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	reader := csv.NewReader(file)
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	var new_data [][]string
+	for _, row := range data {
+		if row[0] != id {
+			new_data = append(new_data, row)
+		}
+	}
+
+	// Empty the file and go to start of the file
+	file.Truncate(0)
+	file.Seek(0, 0)
+
+	// Write the new data to the file
+	writer := csv.NewWriter(file)
+	writer.WriteAll(new_data)
+	writer.Flush()
+}
+
 func main() {
 	var makeFileName = flag.StringP("make", "m", "", "The name of file to make")
-	var makeForce = flag.BoolP("make-force", "M", false, "Force overwrite the file")
-	var displayFileName = flag.StringP("display", "d", "", "The name of file to display")
-	var writeFileName = flag.StringP("write", "w", "", "The name of file to write")
-	var writeText = flag.StringP("text", "t", "", "The data to write to the file")
+	var makeFileNameForce = flag.StringP("make-force", "M", "", "The name of file to overwrite")
+	var listFileName = flag.StringP("list", "l", "", "The name of file to display")
+	var writeArgs = flag.StringP("write", "w", "", "The name of file follwed by the text to write")
+	var deleteArgs = flag.StringP("delete", "d", "", "The name of file to delete followed by ID")
 	flag.Parse()
 
 	// To create a file
 	if *makeFileName != "" {
-		makeCSV(*makeFileName, *makeForce)
+		makeCSV(*makeFileName, false)
+	}
+	// To create a file and overwrite if it already exists
+	if *makeFileNameForce != "" {
+		makeCSV(*makeFileNameForce, true)
 	}
 	// To display the contents of a file
-	if *displayFileName != "" {
-		displayFile(*displayFileName)
+	if *listFileName != "" {
+		listFile(*listFileName)
 	}
 
 	// To write to a file
-	if *writeFileName != "" && *writeText != "" {
-		writeLine(*writeFileName, *writeText)
-	} else if *writeFileName != "" {
-		fmt.Println("Please provide data to write to the file")
+	if *writeArgs != "" {
+		args := flag.Args()
+
+		if len(args) == 0 {
+			fmt.Println("Provide text to write")
+			return
+		}
+
+		if len(args) > 1 {
+			fmt.Println("Received more than one arguement. Use double quotes (\") if your text contains space.")
+			return
+		}
+
+		var text string = args[0]
+		writeLine(*writeArgs, text)
+	}
+
+	// To delete a line from a file
+	if *deleteArgs != "" {
+		args := flag.Args()
+
+		if len(args) == 0 {
+			fmt.Println("Provide ID to delete")
+			return
+		}
+
+		if len(args) > 1 {
+			fmt.Println("Supports one ID at a time")
+			return
+		}
+
+		var id string = args[0]
+		deleteTask(*deleteArgs, id)
 	}
 }
